195b96121627395b8606f09456ba30a2e69a1fe3
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2025-07-29 11:28:10 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["fioriflyingdemo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fioriflyingdemo/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
